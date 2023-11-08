f24023dc5ddf9f6299d6783faa2f063e16cb67f7
Nathaniel Nicholas <nn222qn@student.lnu.se>
2023-11-08 01:57:19 +0100

    functions added

--- app.py
+++ app.py
@@ -5,12 +5,39 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    name = 'Nate'
+    return render_template("index.html", name=name)
 
 
-@app.route('/welcome')
-def welcome():
-    return render_template("index.html")
+@app.route('/drinks')
+def drinks():
+    return render_template("drinks.html")
+
+
+@app.route('/drinks/hot')
+def hotdrinks():
+    return render_template("hotdrinks.html")
+
+
+@app.route('/drinks/cold')
+def colddrinks():
+    return render_template("colddrinks.html")
+
+
+@app.route('/food')
+def food():
+    return render_template("food.html")
+
+
+@app.route('/merchandise')
+def merch():
+    return render_template("merchandise.html")
+
+
+
+@app.route('/checkout')
+def checkout():
+    return render_template("checkout.html")
 
 
 if __name__ == '__main__':
